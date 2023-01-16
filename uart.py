04a072d4aad4d2863ab2f3e0104577ae0a8da056
lucasgandac <lucasgandac@hotmail.com>
2023-01-16 04:18:48 +0000

    envio comando via uart modbus

---
+++ uart.py
@@ -0,0 +1,18 @@
+import serial
+import crcmod
+
+uart = serial.Serial('/dev/serial0',9600, 8)
+if (uart == -1):
+    print("Erro - Não foi possível iniciar a UART.\n");
+else:
+    print("UART inicializada!\n");
+
+m1 = b''.join([b'\x01', b'\x23', b'\xC1\x09\x06\x06\x08'])
+
+crc16 = crcmod.predefined.mkCrcFun('crc-16')
+crc = crc16(m1)
+msg = m1 + crc.to_bytes(2, 'little')
+           
+print("escrevendo...")
+uart.write(msg)
+print(msg)
