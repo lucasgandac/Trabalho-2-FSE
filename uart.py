16fd5abf84feea62e694f970bc8b69d0a498b90b
lucasgandac <lucasgandac@hotmail.com>
2023-01-16 04:34:40 +0000

    ativa estado do sistema

--- uart.py
+++ uart.py
@@ -7,7 +7,8 @@ if (uart == -1):
 else:
     print("UART inicializada!\n");
 
-m1 = b''.join([b'\x01', b'\x23', b'\xC1\x09\x06\x06\x08'])
+m1 = b''.join([b'\x01', b'\x16', b'\xC1', b'\x09\x06\x06\x08'])
+#m1 = b''.join([b'\x01', b'\x16', b'\xD3', b'\x09\x06\x06\x08', b'\x00' ])
 
 crc16 = crcmod.predefined.mkCrcFun('crc-16')
 crc = crc16(m1)
