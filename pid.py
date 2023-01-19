0d9a73e3ef821b67baa970f13b5c45423e0ecf24
lucasgandac <lucasgandac@hotmail.com>
2023-01-19 02:02:33 -0300

    adiciona pid a ajusta uart

---
+++ pid.py
@@ -0,0 +1,45 @@
+class PID:
+    def __init__(self):
+        self.saida_medida = 0.0
+        self.sinal_de_controle = 0.0
+        self.referencia = 0.0
+        self.Kp = 0.0  # Ganho Proporcional
+        self.Ki = 0.0  # Ganho Integral
+        self.Kd = 0.0  # Ganho Derivativo
+        self.T = 1.0  # Periodo de Amostragem (ms)
+        self.last_time = 0
+        self.erro_total = 0.0
+        self.erro_anterior = 0.0
+        self.sinal_de_controle_MAX = 100.0
+        self.sinal_de_controle_MIN = -100.0
+
+    def pid_configura_constantes(self, Kp_, Ki_, Kd_):
+        self.Kp = Kp_
+        self.Ki = Ki_
+        self.Kd = Kd_
+
+    def pid_atualiza_referencia(self, referencia_):
+        self.referencia = referencia_
+
+    def pid_controle(self, saida_medida):
+        erro = self.referencia - saida_medida
+        self.erro_total += erro  # Acumula o erro (Termo Integral)
+
+        if self.erro_total >= self.sinal_de_controle_MAX:
+            self.erro_total = self.sinal_de_controle_MAX
+        elif self.erro_total <= self.sinal_de_controle_MIN:
+            self.erro_total = self.sinal_de_controle_MIN
+
+        delta_error = erro - self.erro_anterior  # Diferença entre os erros (Termo Derivativo)
+
+        self.sinal_de_controle = (
+           ( self.Kp * erro )+ ((self.Ki * self.T) * self.erro_total) + ((self.Kd / self.T) * delta_error)
+        )  # PID calcula sinal de controle
+
+        if self.sinal_de_controle >= self.sinal_de_controle_MAX:
+            self.sinal_de_controle = self.sinal_de_controle_MAX
+        elif self.sinal_de_controle <= self.sinal_de_controle_MIN:
+            self.sinal_de_controle = self.sinal_de_controle_MIN
+
+        self.erro_anterior = erro
+        return self.sinal_de_controle
--- uart.py
+++ uart.py
@@ -9,40 +9,52 @@ def inicializaEstado():
     m1 = b''.join([b'\x01', b'\x16', b'\xD3', matricula, b'\x01' ])
     return m1
 
-uart = serial.Serial('/dev/serial0',9600, 8)
-if (uart == -1):
-    print("Erro - Não foi possível iniciar a UART.\n");
-else:
-    print("UART inicializada!\n");
+def initUart():
+    uart = serial.Serial('/dev/serial0',9600, 8)
+    if (uart == -1):
+        print("Erro - Não foi possível iniciar a UART.\n");
+    else:
+        print("UART inicializada!\n");
+    return uart
 
+def initEstado(uart):
+    estado = b''.join([b'\x01', b'\x16', b'\xD3', matricula, b'\x00' ])
+    funcionamento = b''.join([b'\x01', b'\x16', b'\xD4', matricula, b'\x00' ])
+    controleTemp = b''.join([b'\x01', b'\x16', b'\xD5', matricula, b'\x00' ])
+    msg1 = adicionaCRC(estado)
+    msg2 = adicionaCRC(funcionamento)
+    msg3 = adicionaCRC(controleTemp)
+    uart.write(msg1)
+    uart.write(msg2)
+    uart.write(msg3)
+
+def adicionaCRC(mensagem):
+    crc16 = crcmod.predefined.mkCrcFun('crc-16')
+    crc = crc16(mensagem)
+    crc = crc.to_bytes(2, 'little')
+    mensagem = mensagem + crc
+    return mensagem
+    
+uart = initUart()
+initEstado(uart)
 m1 = b''.join([b'\x01', b'\x16', b'\xC1', b'\x09\x06\x06\x08'])
-#m1 = b''.join([b'\x01', b'\x16', b'\xD3', b'\x09\x06\x06\x08', b'\x00' ])
 
 crc16 = crcmod.predefined.mkCrcFun('crc-16')
 crc = crc16(m1)
 msg = m1 + crc.to_bytes(2, 'little')
-
-message = inicializaEstado()  
-crc1 = crc16(message)
-message = message + crc1.to_bytes(2, 'little')
-print("escrevendo...")
-#uart.write(msg)
-#uart.write(message)
-#response = uart.read(4) # read the response
-#print(response)
-#last_byte = response[-1]
-#float_data = struct.unpack('f', response)[-1]
-#print(last_byte)
 print(msg)
+print(adicionaCRC(m1))
 
-
-""" m3 = b''.join([b'\x01', b'\x23', b'\xC3', b'\x09\x06\x06\x08'])
+m3 = b''.join([b'\x01', b'\x23', b'\xC3', b'\x09\x06\x06\x08'])
 crc16 = crcmod.predefined.mkCrcFun('crc-16')
 crc3 = crc16(m3)
 msg = m3 + crc3.to_bytes(2, 'little')
-while True:
+uart.write(msg)
+response = uart.read(9)
+print(response)
+'''while True:
     uart.write(message)
     if uart.inWaiting() > 0:
         response = uart.read(4)
         print(response)
-    time.sleep(0.5) """
+    time.sleep(0.5)'''
