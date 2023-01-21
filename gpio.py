47f6074fc78aad2c747364c3807c922cffa490d0
lucasgandac <lucasgandac@hotmail.com>
2023-01-21 06:45:13 +0000

    funcionamento basico do forno

--- gpio.py
+++ gpio.py
@@ -10,15 +10,18 @@ class GpioController:
         gpio.setup(23, gpio.OUT)
         gpio.setup(24, gpio.OUT)
         
-    def ligaResistor(self):
-        dc = 0
-        pwm = gpio.PWM(24,1000)
-        pwm.start(0)
-        while True:
-            pwm.ChangeDutyCycle(100)
+    def ligaResistor(self, dc, pwm):
+        #dc = 0
+        if dc > 100:
+          dc = 100
+        pwm.ChangeDutyCycle(dc)
+        ''' pwm = gpio.PWM(23,1000)
+        pwm.start(dc)'''
+        #while True:
+        #pwm.ChangeDutyCycle(dc)
         '''while True:
-            if dc > 60:
-                dc = 60
+            if dc > 100:
+                 dc = 100
             pwm.ChangeDutyCycle(dc)
             dc += 1'''
             
@@ -28,7 +31,14 @@ class GpioController:
         gpio.output(23,False)'''
         
 controleTemp = GpioController()
-controleTemp.ligaResistor()
+pwm = gpio.PWM(24,1000)
+pwm.start(0)
+#pwm.ChangeDutyCycle(100)
+dc = 100
+while True:
+      controleTemp.ligaResistor( dc, pwm)
+
+
 #controleTemp.desligaResistor()
 
 '''gpio.setmode(gpio.BCM)
